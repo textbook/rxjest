7e564475a15aa7e498dd397a900676fd5008b7f5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-09-25 13:42:25 +0100

    Drive out implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,23 @@
-import { from } from "rxjs";
+import { from, throwError } from "rxjs";
 
 import { eventually } from "./index";
 
 describe("RxJeSt", () => {
 	describe("eventually", () => {
-		it("resolves to false if no value matches", () => {
-			return expect(eventually(from(["foo", "bar", "baz"]), "qux")).resolves.toBe(false);
+		it("resolves to false if no value matches", async () => {
+			await expect(eventually(from(["foo", "bar", "baz"]), "qux")).resolves.toBe(false);
+		});
+
+		it("resolves to false if no value is emitted", async () => {
+			await expect(eventually(from([]), "qux")).resolves.toBe(false);
+		});
+
+		it("resolves to true if any value matches", async () => {
+			await expect(eventually(from(["foo", "bar", "baz"]), "bar")).resolves.toBe(true);
+		});
+
+		it("resolves to false if the observable errors", async () => {
+			await expect(eventually(throwError(() => "oh no!"), "qux")).resolves.toBe(false);
 		});
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import type { Observable } from "rxjs";
+import { first, firstValueFrom } from "rxjs";
 
 export async function eventually<T>(stream$: Observable<T>, expected: T): Promise<boolean> {
-	return false;
+	return firstValueFrom(stream$.pipe(first((actual) => {
+		try {
+			expect(actual).toEqual(expected);
+			return true;
+		} catch {
+			return false;
+		}
+	}))).then(() => true).catch(() => false);
 }
