e8ef27bc488e4400eaf4826f627e5b7b03c42614
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-01-24 19:07:35 +0000

    Apply in-range dependency updates

--- src/matchers/testUtils.ts
+++ src/matchers/testUtils.ts
@@ -5,6 +5,7 @@ import * as matcherUtils from "jest-matcher-utils";
 chalk.level = 0;
 
 export const createContext: (overrides?: Partial<jest.MatcherUtils>) => jest.MatcherUtils = (overrides) => ({
+	customTesters: [],
 	dontThrow: () => undefined,
 	equals,
 	isNot: false,
